f534927a83d3d9e6e6776fa405b5036250da67eb
MalusciCamp <malusci.gestionale@gmail.com>
2026-02-26 15:12:55 +0100

    Add files via upload

--- js/utility.js
+++ js/utility.js
@@ -6,3 +6,25 @@ const settimanaID = params.get("id");
 if(!settimanaID){
   alert("Settimana non trovata");
 }
+
+function apriMailBox(){
+  document.getElementById("popupMail").style.display="flex";
+}
+
+function chiudiMail(){
+  document.getElementById("popupMail").style.display="none";
+}
+function inviaMailSettimana(){
+
+  const oggetto = document.getElementById("oggettoMail").value.trim();
+  const testo = document.getElementById("testoMail").value.trim();
+
+  if(!oggetto || !testo){
+    alert("Compila oggetto e testo");
+    return;
+  }
+
+  alert("Funzione invio email non ancora attiva.\n\nSistema pronto per configurazione.");
+
+  chiudiMail();
+}
